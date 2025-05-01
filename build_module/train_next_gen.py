d8096882ac907a1ad5d6c75db59ef76b08c7d01a
CWYOC <117840941+CWYOC@users.noreply.github.com>
2025-05-01 05:49:33 +0100

    change

--- build_module/train_next_gen.py
+++ build_module/train_next_gen.py
@@ -19,12 +19,14 @@ class GenePredictorNN(nn.Module):
         super().__init__()
         self.fc1 = nn.Linear(input_size, 512)
         self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, output_size)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, output_size)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        return self.fc3(x)
+        x = torch.relu(self.fc3(x))
+        return self.fc4(x)
 
 # --- Setup ---
 csv_dir = "/Users/bearcheung/Documents/Year3/FYP/sample_families"
